5a50a24a2bc90ca1d652ea6b5ba4ff094776b451
David <dbarbour2@cnm.edu>
2025-03-03 10:50:12 -0700

    refactoring calendar ui

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/CalendarFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/CalendarFragment.java
@@ -9,27 +9,47 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.adapter.DayBinder;
 import edu.cnm.deepdive.nasaapod.databinding.FragmentCalendarBinding;
 import edu.cnm.deepdive.nasaapod.viewmodel.ApodViewModel;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.Month;
 import java.time.YearMonth;
 import java.time.temporal.WeekFields;
 import java.util.Locale;
+import javax.inject.Inject;
 
 @AndroidEntryPoint
 public class CalendarFragment extends Fragment {
 
+  private static final String TAG = CalendarFragment.class.getSimpleName();
+  
+  @Inject
+  DayBinder dayBinder;
+  // TODO: 3/3/2025 add injected header binder 
+  
   private FragmentCalendarBinding binding;
   private ApodViewModel viewModel;
-
+  private YearMonth selectedMonth;
+  
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater,
       @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    LocalDate firstApodDate = LocalDate.parse(getString(R.string.first_apod_date));
+    YearMonth firstApodMonth = YearMonth.from(firstApodDate);
+    DayOfWeek firstDayOfWeek = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
+    YearMonth currentMonth = YearMonth.now();
+    
+    // TODO: 3/3/2025 attach a listener to day binder
     binding = FragmentCalendarBinding.inflate(inflater, container, false);
-    // TODO: 2025-02-28 Initialize UI.
+    binding.calendar.setDayBinder(dayBinder);
+    // TODO: 3/3/2025 set month header binding on calendar
+    binding.calendar.setup(firstApodMonth,currentMonth,firstDayOfWeek);
+    // TODO: 3/3/2025 set a month scroll listener 
+    
     return binding.getRoot();
   }
 
